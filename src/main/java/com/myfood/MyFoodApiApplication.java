f4c1400d887685e9e17553e9cc7d51d488c16609
wandersonSantiago <wandersonsantiago86@gmail.com>
2020-02-03 22:14:58 -0300

    first commit

---
+++ src/main/java/com/myfood/MyFoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.myfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFoodApiApplication.class, args);
+	}
+
+}
